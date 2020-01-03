30320eaed65e9382e793c4c5239350878db28556
Jordan Brockopp <Jordan.Brockopp@target.com>
2020-01-03 13:50:35 -0600

    feat: add build struct for plugin

---
+++ cmd/vela-docker/build_test.go
@@ -0,0 +1,43 @@
+package main
+
+import "testing"
+
+func TestDocker_Build_Validate(t *testing.T) {
+	// setup types
+	b := &Build{
+		Event: "push",
+		Sha:   "7fd1a60b01f91b314f59955a4e4d4e80d8edf11d",
+		Tag:   "v0.0.0",
+	}
+
+	err := b.Validate()
+	if err != nil {
+		t.Errorf("Validate returned err: %v", err)
+	}
+}
+
+func TestDocker_Build_Validate_NoEvent(t *testing.T) {
+	// setup types
+	b := &Build{
+		Sha: "7fd1a60b01f91b314f59955a4e4d4e80d8edf11d",
+		Tag: "v0.0.0",
+	}
+
+	err := b.Validate()
+	if err == nil {
+		t.Errorf("Validate should have returned err")
+	}
+}
+
+func TestDocker_Build_Validate_NoSha(t *testing.T) {
+	// setup types
+	b := &Build{
+		Event: "push",
+		Tag:   "v0.0.0",
+	}
+
+	err := b.Validate()
+	if err == nil {
+		t.Errorf("Validate should have returned err")
+	}
+}
