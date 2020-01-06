637c0904b3c89c7cff3ffa9ac19e65bcf7450653
Jordan Brockopp <Jordan.Brockopp@target.com>
2020-01-06 08:46:50 -0600

    feat: add image struct for plugin

---
+++ cmd/vela-docker/image.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/sirupsen/logrus"
+)
+
+// Image represents the CLI configuration for image information.
+type Image struct {
+	// variables passed to the image at build-time
+	Args []string
+	// path to the context for building the image
+	Context string
+	// path to the file for building the image
+	Dockerfile string
+}
+
+// Validate verifies the Image is properly configured.
+func (i *Image) Validate() error {
+	logrus.Trace("validating image plugin configuration")
+
+	if len(i.Context) == 0 {
+		return fmt.Errorf("no image context provided")
+	}
+
+	if len(i.Dockerfile) == 0 {
+		return fmt.Errorf("no image dockerfile provided")
+	}
+
+	return nil
+}
