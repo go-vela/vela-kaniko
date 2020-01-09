e2c07f46103b9445ef1de558e0294fe83ffba7ec
Jordan Brockopp <jordan.brockopp@target.com>
2020-01-08 22:54:43 -0600

    feat: add exec method for plugin struct (#16)
    * feat: add exec helper function
    
    * feat: add exec method for plugin struct

--- cmd/vela-docker/plugin_test.go
+++ cmd/vela-docker/plugin_test.go
@@ -5,11 +5,14 @@
 package main
 
 import (
+	"os/exec"
 	"reflect"
 	"testing"
+
+	"github.com/spf13/afero"
 )
 
-func TestDocker_Plugin_Exec(t *testing.T) {
+func TestDocker_Plugin_Exec_BadWrite(t *testing.T) {
 	// setup types
 	p := &Plugin{
 		Build: &Build{
@@ -38,107 +41,15 @@ func TestDocker_Plugin_Exec(t *testing.T) {
 	}
 
 	err := p.Exec()
-	if err != nil {
-		t.Errorf("Exec returned err: %v", err)
+	if err == nil {
+		t.Errorf("Exec should have returned err")
 	}
 }
 
-func TestDocker_Plugin_Flags(t *testing.T) {
-	// setup types
-	p := &Plugin{
-		Build: &Build{
-			Event: "tag",
-			Sha:   "7fd1a60b01f91b314f59955a4e4d4e80d8edf11d",
-			Tag:   "v0.0.0",
-		},
-		Image: &Image{
-			Args:       []string{"foo=bar"},
-			Context:    ".",
-			Dockerfile: "Dockerfile",
-		},
-		Registry: &Registry{
-			Name:     "index.docker.io",
-			Username: "octocat",
-			Password: "superSecretPassword",
-			DryRun:   true,
-		},
-		Repo: &Repo{
-			Cache:     true,
-			CacheName: "index.docker.io/target/vela-docker",
-			Name:      "index.docker.io/target/vela-docker",
-			Tags:      []string{"latest"},
-			AutoTag:   true,
-		},
-	}
+func TestDocker_Plugin_Exec_BadExec(t *testing.T) {
+	// setup filesystem
+	appFS = afero.NewMemMapFs()
 
-	want := []string{
-		"--build-arg=foo=bar",
-		"--cache",
-		"--cache-repo=index.docker.io/target/vela-docker",
-		"--context=.",
-		"--destination=index.docker.io/target/vela-docker:latest",
-		"--destination=index.docker.io/target/vela-docker:v0.0.0",
-		"--dockerfile=Dockerfile",
-		"--no-push",
-		"--verbosity=info",
-	}
-
-	// run test
-	got := p.Flags()
-
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("Flags is %v, want %v", got, want)
-	}
-}
-
-func TestDocker_Plugin_Flags_NoCacheRepo(t *testing.T) {
-	// setup types
-	p := &Plugin{
-		Build: &Build{
-			Event: "push",
-			Sha:   "7fd1a60b01f91b314f59955a4e4d4e80d8edf11d",
-			Tag:   "v0.0.0",
-		},
-		Image: &Image{
-			Args:       []string{"foo=bar"},
-			Context:    ".",
-			Dockerfile: "Dockerfile",
-		},
-		Registry: &Registry{
-			Name:     "index.docker.io",
-			Username: "octocat",
-			Password: "superSecretPassword",
-			DryRun:   true,
-		},
-		Repo: &Repo{
-			Cache:   true,
-			Name:    "index.docker.io/target/vela-docker",
-			Tags:    []string{"latest"},
-			AutoTag: true,
-		},
-	}
-
-	want := []string{
-		"--build-arg=foo=bar",
-		"--cache",
-		"--cache-repo=index.docker.io/target/vela-docker",
-		"--context=.",
-		"--destination=index.docker.io/target/vela-docker:latest",
-		"--destination=index.docker.io/target/vela-docker:7fd1a60b01f91b314f59955a4e4d4e80d8edf11d",
-		"--dockerfile=Dockerfile",
-		"--no-push",
-		"--verbosity=info",
-	}
-
-	// run test
-	got := p.Flags()
-
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("Flags is %v, want %v", got, want)
-	}
-}
-
-func TestDocker_Plugin_Flags_NoDryRun(t *testing.T) {
 	// setup types
 	p := &Plugin{
 		Build: &Build{
@@ -166,7 +77,139 @@ func TestDocker_Plugin_Flags_NoDryRun(t *testing.T) {
 		},
 	}
 
-	want := []string{
+	err := p.Exec()
+	if err == nil {
+		t.Errorf("Exec should have returned err")
+	}
+}
+
+func TestDocker_Plugin_Command(t *testing.T) {
+	// setup types
+	p := &Plugin{
+		Build: &Build{
+			Event: "tag",
+			Sha:   "7fd1a60b01f91b314f59955a4e4d4e80d8edf11d",
+			Tag:   "v0.0.0",
+		},
+		Image: &Image{
+			Args:       []string{"foo=bar"},
+			Context:    ".",
+			Dockerfile: "Dockerfile",
+		},
+		Registry: &Registry{
+			Name:     "index.docker.io",
+			Username: "octocat",
+			Password: "superSecretPassword",
+			DryRun:   true,
+		},
+		Repo: &Repo{
+			Cache:     true,
+			CacheName: "index.docker.io/target/vela-docker",
+			Name:      "index.docker.io/target/vela-docker",
+			Tags:      []string{"latest"},
+			AutoTag:   true,
+		},
+	}
+
+	want := exec.Command(
+		kanikoBin,
+		"--build-arg=foo=bar",
+		"--cache",
+		"--cache-repo=index.docker.io/target/vela-docker",
+		"--context=.",
+		"--destination=index.docker.io/target/vela-docker:latest",
+		"--destination=index.docker.io/target/vela-docker:v0.0.0",
+		"--dockerfile=Dockerfile",
+		"--no-push",
+		"--verbosity=info",
+	)
+
+	// run test
+	got := p.Command()
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Command is %v, want %v", got, want)
+	}
+}
+
+func TestDocker_Plugin_Command_NoCacheRepo(t *testing.T) {
+	// setup types
+	p := &Plugin{
+		Build: &Build{
+			Event: "push",
+			Sha:   "7fd1a60b01f91b314f59955a4e4d4e80d8edf11d",
+			Tag:   "v0.0.0",
+		},
+		Image: &Image{
+			Args:       []string{"foo=bar"},
+			Context:    ".",
+			Dockerfile: "Dockerfile",
+		},
+		Registry: &Registry{
+			Name:     "index.docker.io",
+			Username: "octocat",
+			Password: "superSecretPassword",
+			DryRun:   true,
+		},
+		Repo: &Repo{
+			Cache:   true,
+			Name:    "index.docker.io/target/vela-docker",
+			Tags:    []string{"latest"},
+			AutoTag: true,
+		},
+	}
+
+	want := exec.Command(
+		kanikoBin,
+		"--build-arg=foo=bar",
+		"--cache",
+		"--cache-repo=index.docker.io/target/vela-docker",
+		"--context=.",
+		"--destination=index.docker.io/target/vela-docker:latest",
+		"--destination=index.docker.io/target/vela-docker:7fd1a60b01f91b314f59955a4e4d4e80d8edf11d",
+		"--dockerfile=Dockerfile",
+		"--no-push",
+		"--verbosity=info",
+	)
+
+	// run test
+	got := p.Command()
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Command is %v, want %v", got, want)
+	}
+}
+
+func TestDocker_Plugin_Command_NoDryRun(t *testing.T) {
+	// setup types
+	p := &Plugin{
+		Build: &Build{
+			Event: "push",
+			Sha:   "7fd1a60b01f91b314f59955a4e4d4e80d8edf11d",
+			Tag:   "v0.0.0",
+		},
+		Image: &Image{
+			Args:       []string{"foo=bar"},
+			Context:    ".",
+			Dockerfile: "Dockerfile",
+		},
+		Registry: &Registry{
+			Name:     "index.docker.io",
+			Username: "octocat",
+			Password: "superSecretPassword",
+			DryRun:   false,
+		},
+		Repo: &Repo{
+			Cache:     true,
+			CacheName: "index.docker.io/target/vela-docker",
+			Name:      "index.docker.io/target/vela-docker",
+			Tags:      []string{"latest"},
+			AutoTag:   true,
+		},
+	}
+
+	want := exec.Command(
+		kanikoBin,
 		"--build-arg=foo=bar",
 		"--cache",
 		"--cache-repo=index.docker.io/target/vela-docker",
@@ -175,13 +218,13 @@ func TestDocker_Plugin_Flags_NoDryRun(t *testing.T) {
 		"--destination=index.docker.io/target/vela-docker:7fd1a60b01f91b314f59955a4e4d4e80d8edf11d",
 		"--dockerfile=Dockerfile",
 		"--verbosity=info",
-	}
+	)
 
 	// run test
-	got := p.Flags()
+	got := p.Command()
 
 	if !reflect.DeepEqual(got, want) {
-		t.Errorf("Flags is %v, want %v", got, want)
+		t.Errorf("Command is %v, want %v", got, want)
 	}
 }
 
