811b0ae88c3b69688628aa7bab94d28af4a7228c
kneal <neal.coleman@target.com>
2020-07-28 11:23:22 -0500

    fix: update params pathing

--- cmd/vela-docker/main.go
+++ cmd/vela-docker/main.go
@@ -42,7 +42,7 @@ func main() {
 
 		&cli.StringFlag{
 			EnvVars:  []string{"PARAMETER_LOG_LEVEL", "VELA_LOG_LEVEL", "DOCKER_LOG_LEVEL"},
-			FilePath: string("/vela/secrets/parameter/log_level,/vela/secrets/docker/log_level"),
+			FilePath: string("/vela/parameters/log_level,/vela/secrets/docker/log_level"),
 			Name:     "log.level",
 			Usage:    "set log level - options: (trace|debug|info|warn|error|fatal|panic)",
 			Value:    "info",
@@ -52,19 +52,19 @@ func main() {
 
 		&cli.StringFlag{
 			EnvVars:  []string{"PARAMETER_EVENT", "BUILD_EVENT"},
-			FilePath: string("/vela/secrets/parameter/build/event,/vela/secrets/docker/build/event"),
+			FilePath: string("/vela/parameters/build/event,/vela/secrets/docker/build/event"),
 			Name:     "build.event",
 			Usage:    "event triggered for build",
 		},
 		&cli.StringFlag{
 			EnvVars:  []string{"PARAMETER_SHA", "BUILD_COMMIT"},
-			FilePath: string("/vela/secrets/parameter/build/sha,/vela/secrets/docker/build/sha"),
+			FilePath: string("/vela/parameters/build/sha,/vela/secrets/docker/build/sha"),
 			Name:     "build.sha",
 			Usage:    "commit SHA-1 hash for build",
 		},
 		&cli.StringFlag{
 			EnvVars:  []string{"PARAMETER_TAG", "BUILD_TAG"},
-			FilePath: string("/vela/secrets/parameter/build/tag,/vela/secrets/docker/build/tag"),
+			FilePath: string("/vela/parameters/build/tag,/vela/secrets/docker/build/tag"),
 			Name:     "build.tag",
 			Usage:    "full tag reference for build (only populated for tag events)",
 		},
@@ -73,27 +73,27 @@ func main() {
 
 		&cli.StringSliceFlag{
 			EnvVars:  []string{"PARAMETER_BUILD_ARGS", "IMAGE_BUILD_ARGS"},
-			FilePath: string("/vela/secrets/parameter/image/build_args,/vela/secrets/docker/image/build_args"),
+			FilePath: string("/vela/parameters/image/build_args,/vela/secrets/docker/image/build_args"),
 			Name:     "image.build_args",
 			Usage:    "variables passed to the image at build-time",
 		},
 		&cli.StringFlag{
 			EnvVars:  []string{"PARAMETER_CONTEXT", "IMAGE_CONTEXT"},
-			FilePath: string("/vela/secrets/parameter/image/context,/vela/secrets/docker/image/context"),
+			FilePath: string("/vela/parameters/image/context,/vela/secrets/docker/image/context"),
 			Name:     "image.context",
 			Usage:    "path on local filesystem for building image from",
 			Value:    ".",
 		},
 		&cli.StringFlag{
 			EnvVars:  []string{"PARAMETER_DOCKERFILE", "IMAGE_DOCKERFILE"},
-			FilePath: string("/vela/secrets/parameter/image/dockerfile,/vela/secrets/docker/image/dockerfile"),
+			FilePath: string("/vela/parameters/image/dockerfile,/vela/secrets/docker/image/dockerfile"),
 			Name:     "image.dockerfile",
 			Usage:    "path to text file with build instructions",
 			Value:    "Dockerfile",
 		},
 		&cli.StringFlag{
 			EnvVars:  []string{"PARAMETER_TARGET", "IMAGE_TARGET"},
-			FilePath: string("/vela/secrets/parameter/image/target,/vela/secrets/docker/image/target"),
+			FilePath: string("/vela/parameters/image/target,/vela/secrets/docker/image/target"),
 			Name:     "image.target",
 			Usage:    "build stage to target for image",
 		},
@@ -102,26 +102,26 @@ func main() {
 
 		&cli.BoolFlag{
 			EnvVars:  []string{"PARAMETER_DRY_RUN", "REGISTRY_DRY_RUN"},
-			FilePath: string("/vela/secrets/parameter/dry_run,/vela/secrets/docker/dry_run"),
+			FilePath: string("/vela/parameters/dry_run,/vela/secrets/docker/dry_run"),
 			Name:     "registry.dry_run",
 			Usage:    "enables building images without publishing to the registry",
 		},
 		&cli.StringFlag{
 			EnvVars:  []string{"PARAMETER_REGISTRY", "REGISTRY_NAME"},
-			FilePath: string("/vela/secrets/parameter/registry/name,/vela/secrets/docker/registry/name"),
+			FilePath: string("/vela/parameters/registry/name,/vela/secrets/docker/registry/name"),
 			Name:     "registry.name",
 			Usage:    "Docker registry name to communicate with",
 			Value:    "index.docker.io",
 		},
 		&cli.StringFlag{
 			EnvVars:  []string{"PARAMETER_USERNAME", "REGISTRY_USERNAME", "DOCKER_USERNAME"},
-			FilePath: string("/vela/secrets/parameter/registry/username,/vela/secrets/docker/registry/username,/vela/secrets/docker/username"),
+			FilePath: string("/vela/parameters/registry/username,/vela/secrets/docker/registry/username,/vela/secrets/docker/username"),
 			Name:     "registry.username",
 			Usage:    "user name for communication with the registry",
 		},
 		&cli.StringFlag{
 			EnvVars:  []string{"PARAMETER_PASSWORD", "REGISTRY_PASSWORD", "DOCKER_PASSWORD"},
-			FilePath: string("/vela/secrets/parameter/registry/password,/vela/secrets/docker/registry/password,/vela/secrets/docker/password"),
+			FilePath: string("/vela/parameters/registry/password,/vela/secrets/docker/registry/password,/vela/secrets/docker/password"),
 			Name:     "registry.password",
 			Usage:    "password for communication with the registry",
 		},
@@ -130,38 +130,38 @@ func main() {
 
 		&cli.BoolFlag{
 			EnvVars:  []string{"PARAMETER_AUTO_TAG", "REPO_AUTO_TAG"},
-			FilePath: string("/vela/secrets/parameter/repo/auto_tag,/vela/secrets/docker/repo/auto_tag"),
+			FilePath: string("/vela/parameters/repo/auto_tag,/vela/secrets/docker/repo/auto_tag"),
 			Name:     "repo.auto_tag",
 			Usage:    "enables automatically providing tags for the image",
 		},
 		&cli.BoolFlag{
 			EnvVars:  []string{"PARAMETER_CACHE", "REPO_CACHE"},
-			FilePath: string("/vela/secrets/parameter/repo/cache,/vela/secrets/docker/repo/cache"),
+			FilePath: string("/vela/parameters/repo/cache,/vela/secrets/docker/repo/cache"),
 			Name:     "repo.cache",
 			Usage:    "enables caching of each layer for the image",
 		},
 		&cli.StringFlag{
 			EnvVars:  []string{"PARAMETER_CACHE_REPO", "REPO_CACHE_NAME"},
-			FilePath: string("/vela/secrets/parameter/repo/cache_name,/vela/secrets/docker/repo/cache_name"),
+			FilePath: string("/vela/parameters/repo/cache_name,/vela/secrets/docker/repo/cache_name"),
 			Name:     "repo.cache_name",
 			Usage:    "enables caching of each layer for a specific repo for the image",
 		},
 		&cli.StringFlag{
 			EnvVars:  []string{"PARAMETER_REPO", "REPO_NAME"},
-			FilePath: string("/vela/secrets/parameter/repo/name,/vela/secrets/docker/repo/name"),
+			FilePath: string("/vela/parameters/repo/name,/vela/secrets/docker/repo/name"),
 			Name:     "repo.name",
 			Usage:    "repository name for the image",
 		},
 		&cli.StringSliceFlag{
 			EnvVars:  []string{"PARAMETER_TAGS", "REPO_TAGS"},
-			FilePath: string(".tags,/vela/secrets/parameter/repo/tags,/vela/secrets/docker/repo/tags"),
+			FilePath: string(".tags,/vela/parameters/repo/tags,/vela/secrets/docker/repo/tags"),
 			Name:     "repo.tags",
 			Usage:    "repository tags of the image",
 			Value:    cli.NewStringSlice("latest"),
 		},
 		&cli.StringSliceFlag{
 			EnvVars:  []string{"PARAMETER_LABELS", "REPO_LABELS"},
-			FilePath: string("/vela/secrets/parameter/repo/labels,/vela/secrets/docker/repo/labels"),
+			FilePath: string("/vela/parameters/repo/labels,/vela/secrets/docker/repo/labels"),
 			Name:     "repo.labels",
 			Usage:    "repository labels of the image",
 		},
