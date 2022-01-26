10529c746e492f220ee6bc32ecac452207bf8c69
tgtajuckel <98363772+tgtajuckel@users.noreply.github.com>
2022-01-26 11:43:19 -0600

    feat: Add ability to set `--customPlatform` flag (#110)
    * feat: Add ability to set `--customPlatform` flag
    
    Kaniko supports a `--customPlatform` flag to allow creation of images
    with a different platform than the default, which is a platform derived
    from the build host.
    
    The `--customPlatform` feature doesn't provide any cross-compilation
    support by itself, and only platforms which the underlying build host
    is able to provide will work correctly.
    
    This commit provides a `custom_platform` plugin parameter which,
    when supplied, will provide a value for the `--customPlatform`
    argument for `/kaniko/executor`. There is no attempt to verify that
    the platform requested is supported by the build host.
    
    Should close go-vela/community#477
    
    Example:
    
      steps:
        - name: publish_hello-world_amd64
          image: vela-plugins/kaniko:vCustomPlatform
          pull: always
          parameters:
            repo: vela/hello-world
            custom_platform: linux/amd64
            tags:
              - latest_linux_amd64
        - name: publish_hello-world_arm64
          image: vela-plugins/kaniko:vCustomPlatform
          pull: always
          parameters:
            repo: vela/hello-world
            custom_platform: linux/arm64/v8
            tags:
              - latest_linux_arm64_v8
    
    * Add documentation for custom_platform
    
    * Reformat code

--- cmd/vela-kaniko/plugin_test.go
+++ cmd/vela-kaniko/plugin_test.go
@@ -414,6 +414,68 @@ func TestDocker_Plugin_Command_NoDryRun(t *testing.T) {
 	}
 }
 
+func TestDocker_Plugin_Command_CustomPlatform(t *testing.T) {
+	// setup types
+	p := &Plugin{
+		Build: &Build{
+			Event: "tag",
+			Sha:   "7fd1a60b01f91b314f59955a4e4d4e80d8edf11d",
+			Tag:   "v0.0.0",
+		},
+		Image: &Image{
+			Args:           []string{"foo=bar"},
+			Context:        ".",
+			Dockerfile:     "Dockerfile",
+			Target:         "foo",
+			CustomPlatform: "linux/arm64/v8",
+		},
+		Registry: &Registry{
+			Name:               "index.docker.io",
+			Username:           "octocat",
+			Password:           "superSecretPassword",
+			DryRun:             true,
+			PushRetry:          1,
+			InsecureRegistries: []string{"insecure.docker.local", "docker.local"},
+			InsecurePull:       true,
+			InsecurePush:       true,
+		},
+		Repo: &Repo{
+			Cache:     true,
+			CacheName: "index.docker.io/target/vela-kaniko",
+			Name:      "index.docker.io/target/vela-kaniko",
+			Tags:      []string{"latest"},
+			AutoTag:   true,
+		},
+	}
+
+	want := exec.Command(
+		kanikoBin,
+		"--build-arg=foo=bar",
+		"--cache",
+		"--cache-repo=index.docker.io/target/vela-kaniko",
+		"--context=.",
+		"--destination=index.docker.io/target/vela-kaniko:latest",
+		"--destination=index.docker.io/target/vela-kaniko:v0.0.0",
+		"--dockerfile=Dockerfile",
+		"--no-push",
+		"--push-retry=1",
+		"--target=foo",
+		"--customPlatform=linux/arm64/v8",
+		"--insecure-registry=insecure.docker.local",
+		"--insecure-registry=docker.local",
+		"--insecure-pull",
+		"--insecure",
+		"--verbosity=info",
+	)
+
+	// run test
+	got := p.Command()
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Command is %v, want %v", got, want)
+	}
+}
+
 func TestDocker_Plugin_Validate(t *testing.T) {
 	// setup types
 	p := &Plugin{
