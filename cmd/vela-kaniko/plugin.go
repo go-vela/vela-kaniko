d3aa09a28f76afd68584cf6b1f8e8765a35bf387
wass3r <1301201+wass3r@users.noreply.github.com>
2023-05-26 08:07:10 -0500

    no cuddle

--- cmd/vela-kaniko/plugin.go
+++ cmd/vela-kaniko/plugin.go
@@ -60,6 +60,7 @@ func (p *Plugin) Command() *exec.Cmd {
 	if len(p.Build.TarPath) > 0 {
 		flags = append(flags, fmt.Sprintf("--tar-path=%s", p.Build.TarPath))
 	}
+
 	if p.Build.SingleSnapshot {
 		flags = append(flags, "--single-snapshot")
 	}
