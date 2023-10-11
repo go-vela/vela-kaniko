278ae9fb7f95352bd55f5ba116b17a49bb652c54
David May <49894298+wass3rw3rk@users.noreply.github.com>
2023-10-11 09:14:03 -0500

    chore(license): update source code headers + copyright year (#179)

--- cmd/vela-kaniko/main.go
+++ cmd/vela-kaniko/main.go
@@ -1,6 +1,4 @@
-// Copyright (c) 2022 Target Brands, Inc. All rights reserved.
-//
-// Use of this source code is governed by the LICENSE file in this repository.
+// SPDX-License-Identifier: Apache-2.0
 
 package main
 
@@ -40,7 +38,7 @@ func main() {
 	app.Name = "vela-kaniko"
 	app.HelpName = "vela-kaniko"
 	app.Usage = "Vela Kaniko plugin for building and publishing images"
-	app.Copyright = "Copyright (c) 2022 Target Brands, Inc. All rights reserved."
+	app.Copyright = "Copyright 2019 Target Brands, Inc. All rights reserved."
 	app.Authors = []*cli.Author{
 		{
 			Name:  "Vela Admins",
